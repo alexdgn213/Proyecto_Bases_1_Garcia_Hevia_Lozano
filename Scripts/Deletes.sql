6653d529e20c708b5279810fc54ce969022af220
Alexander Garcia <alexdgn213@gmail.com>
2017-12-02 21:39:37 -0400

    agregados deletes

---
+++ Scripts/Deletes.sql
@@ -0,0 +1,35 @@
+--Deben ir al reves de los creates
+-- Faltan vaciar las secuencias
+delete from pru_mat_est;
+delete from pru_pie_est;
+delete from estatus;
+delete from per_pru_pie;
+delete from mat_inv;
+delete from pru_pie;
+delete from pru_mat;
+delete from prueba;
+delete from tip_mod;
+delete from pieza;
+delete from inventario;
+delete from tip_mat;
+delete from tipo_pieza;
+delete from mat_pro;
+delete from material;
+delete from solicitud;
+delete from aeronave;
+delete from mod_car;
+delete from caracteristica;
+delete from modelo_aeronave;
+delete from informacion_contacto;
+delete from cliente;
+delete from proveedor;
+delete from beneficiario;
+delete from personal;
+delete from zona;
+delete from fabrica;
+delete from lugar;
+
+
+
+
+
