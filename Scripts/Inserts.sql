9c53de4610660a36374b7c7f80c5fa75a9fc31a5
marcolbx <marcolbx@hotmail.com>
2017-12-10 18:18:01 -0400

    Update Inserts
    Ya algunas características están 100% LISTAS

--- Scripts/Inserts.sql
+++ Scripts/Inserts.sql
@@ -1672,7 +1672,7 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
 (35,'Empuje unitario'); 
  
  INSERT INTO mod_car (mod_car_valor,mod_car_descripcion,fk_mod_codigo,fk_car_codigo) VALUES
- (179,'Personas',1,1), --CAPACIDADES ( hay clase densa,alta densa, estandar... varios tipos de clases, no entiendo como llenarlas
+ (179,'Personas',1,1), --CAPACIDADES LISTAS
  (189,'Personas',5,1),
  (118,'Personas',6,1),
  (168,'Personas',7,1),
@@ -1703,9 +1703,11 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
  (366,'Personas',32,1),--AU747Platinum 3 clases
  (412,'Personas',33,1),--gold 3 clases
  (416,'Personas',34,1),--Silver 3 clases
+ (416,'Personas',35,1),-- Silver 3 clases + er
+ (524,'Personas',36,1),
  
  
- (44.1,'metros',1,2), --Longitudes
+ (44.1,'metros',1,2), --Longitudes LISTAS
  (46.7,'metros',5,2),
  (28.6,'metros',6,2),
  (36.5,'metros',7,2),
@@ -1714,13 +1716,32 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
  (33.6,'metros',10,2),
  (39.5,'metros',11,2),
  (42.1,'metros',12,2),
-  (70.66,'metros',13,2),
+ (70.66,'metros',13,2),
  (70.66,'metros',14,2),
  (70.66,'metros',15,2),
  (70.66,'metros',16,2),
+ (70.66,'metros',34,2),
+ (70.66,'metros',35,2),
+ (70.66,'metros',36,2),
  (76.4,'metros',17,2),
+ (28.6,'metros',18,2),
+ (36.5,'metros',19,2),
+ (31.1,'metros',20,2),
+ (31.2,'metros',21,2),
+ (39.5,'metros',22,2),
+ (42.1,'metros',23,2),
+ (42.1,'metros',24,2),
+ (39.5,'metros',25,2),
+ (33.6,'metros',26,2),
+ (33.6,'metros',27,2),
+ (33.6,'metros',28,2),
+ (33.6,'metros',29,2),
+ (33.6,'metros',30,2),
+ (70.66,'metros',31,2),
+ (70.66,'metros',32,2),
+ (70.66,'metros',33,2),
   
- (39.9,'metros',1,3),--Envergaduras
+ (39.9,'metros',1,3),--Envergaduras Listas
  (32.9,'metros',5,3),
  (28.3,'metros',6,3), 
  (28.9,'metros',7,3),
@@ -1733,9 +1754,31 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
  (59.64,'metros',14,3),
  (59.64,'metros',15,3),
  (64.4,'metros',16,3),
+ (64.4,'metros',34,3),
+ (64.4,'metros',35,3),
+ (64.4,'metros',36,3),
  (68.5,'metros',17,3),
-  
- (12.9,'metros',1,4), --Alturas
+ (28.3,'metros',18,3),
+ (28.9,'metros',19,3),
+ (28.9,'metros',20,3),
+ (35.7,'metros',21,3), 
+ (35.7,'metros',22,3),
+ (35.7,'metros',23,3),
+ (35.7,'metros',24,3),
+ (35.7,'metros',25,3),
+ (35.7,'metros',26,3),
+ (35.7,'metros',27,3),
+ (35.7,'metros',28,3),
+ (35.7,'metros',29,3),
+ (35.7,'metros',30,3),
+ (59.64,'metros',31,3),
+ (59.64,'metros',32,3),
+ (59.64,'metros',33,3),
+ 
+ 
+ 
+ 
+ (12.9,'metros',1,4), --Alturas LISTAS
  (10.4,'metros',5,4),
  (11.3,'metros',6,4),
  (11.1,'metros',7,4),
@@ -1748,9 +1791,28 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
  (19.3,'metros',14,4),
  (19.3,'metros',15,4),
  (19.4,'metros',16,4),
+ (19.4,'metros',34,4),
+ (19.4,'metros',35,4),
+ (19.4,'metros',36,4),
  (19.4,'metros',17,4),
+ (11.3,'metros',18,4),
+ (11.1,'metros',19,4),
+ (11.1,'metros',20,4),
+ (12.6,'metros',21,4),
+ (12.5,'metros',22,4),
+ (12.5,'metros',23,4),
+ (12.5,'metros',24,4),
+ (12.5,'metros',25,4),
+ (12.5,'metros',26,4),
+ (12.5,'metros',27,4),
+ (12.5,'metros',28,4),
+ (12.5,'metros',29,4),
+ (12.5,'metros',30,4),
+ (19.3,'metros',31,4),
+ (19.3,'metros',32,4),
+ (19.3,'metros',33,4),
  
- (55580,'KG',1,5), --Peso Vacio
+ (55580,'KG',1,5), --Peso Vacio Listos
  (46700,'KG',5,5),
  (28120,'KG',6,5),
  (33200,'KG',7,5),
@@ -1763,11 +1825,30 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
  (174000,'KG',14,5),
  (178100,'KG',15,5),
  (178750,'KG',16,5),-- sin ER (ver enunciado)
+ (178750,'KG',34,5),-- sin ER (ver enunciado)
  (184600,'KG',35,5), --con ER con 3 clases
  (184600,'KG',36,5), --con ER con 2 clases
  (186000,'KG',17,5),
+ (28120,'KG',18,5),
+ (33200,'KG',19,5),
+ (31300,'KG',20,5),
+ (36380,'KG',21,5),
+ (41415,'KG',22,5),
+ (44675,'KG',23,5),
+ (44675,'KG',24,5),
+ (41415,'KG',25,5),
+ (38150,'KG',26,5),
+ (38150,'KG',27,5),
+ (38150,'KG',28,5),
+ (38150,'KG',29,5),
+ (38150,'KG',30,5),
+ (162400,'KG',31,5),
+ (174000,'KG',32,5),
+ (178100,'KG',33,5),
+
+ 
   
-  (116570,'KG',1,6), --Peso maximo al despegue
+  (116570,'KG',1,6), --Peso maximo al despegue LISTOS
   (95028,'KG',5,6),
   (49190,'KG',6,6),
   (68050,'KG',7,6),
@@ -1777,19 +1858,33 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
   (70000,'KG',26,6), --AU802D Basic +STD
   (77500,'KG',27,6),--AU802D ER + densa
   (77500,'KG',28,6),--AU802D ER + STD
+  (70000,'KG',29,6), --AU802D 
+  (70000,'KG',30,6), --AU802D
   (79000,'KG',11,6),
   (85100,'KG',12,6),
   (333400,'KG',13,6),
   (378000,'KG',14,6),
   (378000,'KG',15,6),
-  (397000,'KG',16,6), --AU747Silver  
+  (397000,'KG',16,6), --AU747Silver 
+  (397000,'KG',34,6), --AU747Silver 
   (413000,'KG',35,6),--AU747Silver ER 3 clases
   (413000,'KG',36,6),--AU747Silver ER 2 clases
   (440000,'KG',17,6),
+  (49190,'KG',18,6),
+  (68050,'KG',19,6),
+  (60550,'KG',20,6),
+  (66000,'KG',21,6),
+  (79000,'KG',22,6),
+  (85100,'KG',23,6),
+  (85100,'KG',24,6),
+  (79000,'KG',25,6),
+  (333400,'KG',31,6),
+  (378000,'KG',32,6),
+  (378000,'KG',33,6),
   
   --(,,1,7), --Plantas Motrices == motorizacion??
   
-  (75.6,'KN',1,8),-- Empuje Normal solo estos lo tienen
+  (75.6,'KN',1,8),-- Empuje Normal LISTOS   **solo estos lo tienen
   (77,'KN',5,8),
   
   (907,'km/h a 1000m',5,9), --Velocidad Crucero
@@ -1804,12 +1899,15 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
   (893,'km/h a 10500m',14,9),
   (893,'km/h a 10500m',15,9),
   (913,'km/h a 10500m',16,9), --Silver
+  (913,'km/h a 10500m',34,9), --Silver 3 clases sin ER
   (916,'km/h a 10500m',35,9),--Silver ER 3 clases
   (916,'km/h a 10500m',36,9),--Silver ER 2 clases
   (916,'km/h a 10500m',17,9),
-  (1000,'km/h',1,10), --Velocidad maxima operativa Vno) solo esta
   
-  (6820,'km',1,11), --Alcance solo estos
+  
+  (1000,'km/h',1,10), --Velocidad maxima operativa Vno) LISTAS **solo esta
+  
+  (6820,'km',1,11), --Alcance LISTOS **solo estos
   (4450,'km',5,11),
   
   (7,'Tripulantes',5,12), -- Tripulacion
@@ -1823,10 +1921,13 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
   (3,'Pilot, copiloto y 1 auxiliar de vuelo',13,12), 
   (3,'Pilot, copiloto y 1 auxiliar de vuelo',14,12),
   (3,'Pilot, copiloto y 1 auxiliar de vuelo',15,12),
-  (2,'Pilot y copiloto',16,12),
+  (2,'Pilot y copiloto',16,12), --Los AU747 Silvers
+  (2,'Pilot y copiloto',34,12),
+  (2,'Pilot y copiloto',35,12),
+  (2,'Pilot y copiloto',36,12),
   (2,'Pilot y copiloto',17,12), 
   
-  (153,'m^2',5,13),--Superficie Alar
+  (153,'m^2',5,13),--Superficie Alar LISTAS 
   
   (13000,'metros',5,14), --Techo de Servicio
   (10700,'metros',6,14),
@@ -1837,7 +1938,7 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
   (12500,'metros',11,14),
   (12500,'metros',12,14),
    
-  (14.9,'m/s',5,15), --Regimen de Ascenso
+  (14.9,'m/s',5,15), --Regimen de Ascenso LISTOS
   
   (76,'cm',6,16),--Distancia entre asientos (1 clase, densa)
   (86,'cm',18,16),-- (1 clase, estandar)
@@ -1926,6 +2027,7 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
   (3190,'m',14,24),
   (3320,'m',15,24),
   (3018,'m',16,24),--sin ER
+  (3018,'m',34,24),
   (3090,'m',35,24), --con ER
   (3090,'m',36,24),--con ER
   
@@ -1954,6 +2056,9 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
   (945,'km/h',14,26),
   (945,'km/h',15,26),
   (978,'km/h',16,26),
+  (978,'km/h',34,26),
+  (978,'km/h',35,26),
+  (978,'km/h',36,26),
   --el ultimo no tiene? jajaj
   
   (17860,'litros',6,27),--Máxima capacidad de combustible
@@ -1967,7 +2072,9 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
   (199160,'litros',14,27),
   (199160,'litros',15,27),
   (216840,'litros',16,27),--sin ER
-  --(241140,'litros',16,27), con ER
+  (216840,'litros',34,27),--sin ER
+  (241140,'litros',35,27),--con ER
+  (241140,'litros',36,27),--con ER
   (243120,'litros',17,27),
   /*
   (Pratt & Whitney,'JT8D-7',6,28),--'Motores(x2)
@@ -2014,7 +2121,9 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
   (6850,'nmi',14,33),
   (6700,'nmi',15,33),
   (7260,'nmi',16,33), -- sin ER
-  --(7670,'nmi',16,33), --Con ER
+  (7260,'nmi',34,33), -- sin ER
+  (7670,'nmi',35,33), --Con ER
+  (7670,'nmi',36,33), --Con ER
   (8000,'nmi',17,33);
   
   /*
@@ -2024,7 +2133,8 @@ INSERT INTO caracteristica (car_codigo,car_nombre) VALUES
   (P&W PW 4062,'GE CF6-80C2B5F,RR RB211-524G/H ',16,34),
   --(GE CF6-80C2B5F,'',16,34), con ER
   (GE GEnx-2B67,'',17,34),
-  
+  */
+  /*
   (PW 207,'kN',13,35),--Empuje Unitario
   (RR 223,'kN',13,35), --el 2do del au747plus y asi vamos
   (PW 244,'kN',14,35),
