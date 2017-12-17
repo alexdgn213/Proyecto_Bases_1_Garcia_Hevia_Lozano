e38d52a225ae474fd7482b22359bb169ab112011
marcolbx <marcolbx@hotmail.com>
2017-12-16 23:09:16 -0400

    v

--- AirUCAB/src/Dominio/Pru_mot.java
+++ AirUCAB/src/Dominio/Pru_mot.java
@@ -20,25 +20,25 @@ import javax.swing.JTable;
  *
  * @author LAB_L11
  */
-public class Tip_pru {
+public class Pru_mot {
     
-    int tip_pru_codigo;
-    int fk_tip_codigo;
+    int pru_mot_codigo;
     int fk_pru_codigo;
+    int fk_mot_codigo;
     
 
-    public Tip_pru(int tip_pru_codigo,int fk_tip_codigo,int fk_pru_codigo) {
-        this.tip_pru_codigo = tip_pru_codigo;
-        this.fk_tip_codigo = fk_tip_codigo;
+    public Pru_mot(int pru_mot_codigo,int fk_pru_codigo,int fk_mot_codigo) {
+        this.pru_mot_codigo = pru_mot_codigo;
         this.fk_pru_codigo = fk_pru_codigo;
+        this.fk_mot_codigo = fk_mot_codigo;
     }
     
     public void agregarADB(ConectorDB conector){
         try{
-            String stm = "INSERT INTO Tip_pru(fk_tip_codigo,fk_pru_codigo) VALUES(?,?)";
+            String stm = "INSERT INTO Pru_mot(fk_pru_codigo,fk_mot_codigo) VALUES(?,?)";
             PreparedStatement pst = conector.conexion.prepareStatement(stm);
-            pst.setInt(1, fk_tip_codigo);
-            pst.setInt(2,fk_pru_codigo);
+            pst.setInt(1, fk_pru_codigo);
+            pst.setInt(2,fk_mot_codigo);
             pst.executeUpdate();
             pst.close();
         }catch (SQLException ex){
@@ -48,11 +48,11 @@ public class Tip_pru {
     
     public void modificarEnDB(ConectorDB conector){
         try{
-            String stm = "UPDATE Tip_pru SET fk_tip_codigo = ?,fk_pru_codigo=? WHERE tip_pru_codigo=?";
+            String stm = "UPDATE Pru_mot SET fk_pru_codigo = ?,fk_mot_codigo=? WHERE pru_mot_codigo=?";
             PreparedStatement pst = conector.conexion.prepareStatement(stm);
-            pst.setInt(3, tip_pru_codigo);
-            pst.setInt(1, fk_tip_codigo);
-            pst.setInt(2,fk_pru_codigo);
+            pst.setInt(3, pru_mot_codigo);
+            pst.setInt(1, fk_pru_codigo);
+            pst.setInt(2,fk_mot_codigo);
             pst.executeUpdate();
             pst.close();
         }catch (SQLException ex){
@@ -62,9 +62,9 @@ public class Tip_pru {
     
     public void eliminarDeDB(ConectorDB conector){
         try{
-            String stm = "Delete from Tip_pru where tip_pru_codigo=?";
+            String stm = "Delete from Pru_mot where pru_mot_codigo=?";
             PreparedStatement pst = conector.conexion.prepareStatement(stm);
-            pst.setInt(1, tip_pru_codigo);
+            pst.setInt(1, pru_mot_codigo);
             pst.executeUpdate();
             pst.close();
         }catch (SQLException ex){
@@ -83,63 +83,38 @@ public class Tip_pru {
         return null;
     }
 
-    public static List<Tip_pru> obtenerTodos(ConectorDB conector){
-        List<Tip_pru> pls = new ArrayList<Tip_pru>();
+    public static List<Pru_mot> obtenerTodos(ConectorDB conector){
+        List<Pru_mot> pm = new ArrayList<Pru_mot>();
         try {
-            ResultSet rs = obtenerResultSet(conector,"SELECT tip_pru_codigo as codigo,fk_tip_codigo as Codigo_Tipo_Pieza,fk_pru_codigo as Codigo_Prueba, FROM Tip_pru");
+            ResultSet rs = obtenerResultSet(conector,"SELECT pru_mot_codigo as codigo,fk_pru_codigo as Codigo_Prueba,fk_mot_codigo as Codigo_Motor FROM Pru_mot");
             while (rs.next()) {
-                Tip_pru l = new Tip_pru(rs.getInt("tip_pru_codigo"),rs.getInt("fk_tip_codigo"),rs.getInt("fk_pru_codigo"));
-                pls.add(l);
+                Pru_mot l = new Pru_mot(rs.getInt("pru_mot_codigo"),rs.getInt("fk_pru_codigo"),rs.getInt("fk_mot_codigo"));
+                pm.add(l);
             }
         } catch (SQLException ex) {
             System.out.print(ex.toString());
         }
-        return pls;
+        return pm;
     }
     
     public static void llenarTabla(ConectorDB conector, JTable jTable){
-        ResultSet rs =obtenerResultSet(conector,"SELECT tip_pru_codigo as Codigo,fk_tip_codigo as Codigo_Tipo_Pieza,fk_pru_codigo as Codigo_Prueba FROM Tip_pru");
+        ResultSet rs =obtenerResultSet(conector,"SELECT pru_mot_codigo as Codigo,fk_pru_codigo as Codigo_Prueba,fk_mot_codigo as Codigo_Motor FROM Pru_mot");
         AdaptadorSQLUI.llenarTabla(jTable, rs);   
     }
    
     
-    public static Tip_pru buscarPorCodigo(ConectorDB conector, int codigo){
-        Tip_pru tp = null;
+    public static Pru_mot buscarPorCodigo(ConectorDB conector, int codigo){
+        Pru_mot tp = null;
         try {
-            PreparedStatement pst = conector.conexion.prepareStatement("SELECT tip_pru_codigo, fk_tip_codigo,fk_pru_codigo FROM tip_pru WHERE tip_pru_codigo=?");
+            PreparedStatement pst = conector.conexion.prepareStatement("SELECT pru_mot_codigo, fk_pru_codigo,fk_mot_codigo FROM pru_mot WHERE pru_mot_codigo=?");
             pst.setInt(1, codigo);
             ResultSet rs = pst.executeQuery();
             while (rs.next()) {
-                tp = new Tip_pru(rs.getInt("tip_pru_codigo"),rs.getInt("fk_tip_codigo"),rs.getInt("fk_pru_codigo"));
+                tp = new Pru_mot(rs.getInt("pru_mot_codigo"),rs.getInt("fk_pru_codigo"),rs.getInt("fk_mot_codigo"));
             }
         } catch (SQLException ex) {
             System.out.print(ex.toString());
         }
         return tp;
     }
-
-    public int getTip_pru_codigo() {
-        return tip_pru_codigo;
-    }
-
-    public void setTip_pru_codigo(int tip_pru_codigo) {
-        this.tip_pru_codigo = tip_pru_codigo;
-    }
-
-    public int getFk_tip_codigo() {
-        return fk_tip_codigo;
-    }
-
-    public void setFk_tip_codigo(int fk_tip_codigo) {
-        this.fk_tip_codigo = fk_tip_codigo;
-    }
-
-    public int getFk_pru_codigo() {
-        return fk_pru_codigo;
-    }
-
-    public void setFk_pru_codigo(int fk_pru_codigo) {
-        this.fk_pru_codigo = fk_pru_codigo;
-    }
-
 }
