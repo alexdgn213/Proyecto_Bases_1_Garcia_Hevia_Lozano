86047310e6095ce7828efadde7b7f4915aa753d2
vahevia <heviaveronica@gmail.com>
2017-12-17 16:54:38 -0400

    creates con mas cascades

--- AirUCAB/src/Dominio/Inventario.java
+++ AirUCAB/src/Dominio/Inventario.java
@@ -133,5 +133,5 @@ public class Inventario {
         ResultSet rs =obtenerResultSet(conector,"SELECT mat_codigo as Codigo_Material ,mat_nombre as Material,mat_inv_cantidad as Cantidad FROM inventario, material,mat_inv,fabrica where fk_inv_codigo=inv_codigo AND fk_mat_codigo=mat_codigo AND fk_fab_codigo=fab_codigo AND fab_nombre='"+nombre+"'");
         AdaptadorSQLUI.llenarTabla(jTable, rs);   
     }
-   
+     
     }
