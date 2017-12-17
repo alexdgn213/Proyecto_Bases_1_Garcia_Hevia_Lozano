a87c55c936ee413dd811452ec0b61405dc5e687d
marcolbx <marcolbx@hotmail.com>
2017-12-17 19:16:35 -0400

    Update Inserts.sql

--- Scripts/Inserts.sql
+++ Scripts/Inserts.sql
@@ -2715,7 +2715,10 @@ INSERT INTO car_mot(car_mot_valor,car_mot_descripcion,fk_car_codigo,fk_mot_codig
  ('Control de calidad de la pieza','Comprueba el nivel optimo de la pieza ensamblada'),
  ('Prueba de control de ensamble','Comprueba que el ensamblaje se haya hecho satisfactoriamente'),
  ('Control de calidad de piezas por traslado','Comprueba que no hayan sufrido daños por traslado'),
- ('Control de calidad de motor','Comprueba que el ensamblaje del motor se haya hecho satisfactoriamente');
+ ('Control de calidad de motor','Comprueba que el ensamblaje del motor se haya hecho satisfactoriamente'),
+ ('Control de resistencia del motor','Comprueba la resistencia del motor'),
+ ('Armado','Comprueba el armado del avion '),
+ ('Despegue','Comprueba que el despegue es exitoso');
  
  INSERT INTO tip_pru(fk_tip_codigo,fk_pru_codigo) VALUES
  (1,4),
